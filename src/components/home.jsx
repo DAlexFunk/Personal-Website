eadf4ecb20da7a6d8f480811ef1b92900451633d
DAlexFunk <alexdagreat04@gmail.com>
2024-11-27 14:12:28 -0500

    Feat: add paragraph intro to the home page

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,5 +1,5 @@
 import alexImg from "../assets/alexHero.jpg";
-import arrow from "../assets/curvedArrow.svg"
+import arrow from "../assets/curvedArrow.svg";
 
 export default function Home() {
   return (
@@ -14,6 +14,17 @@ export default function Home() {
           <img src={arrow} className="arrow" />
           <img src={alexImg} alt="Alex Funk" className="heroImg" />
         </div>
+      </section>
+      <section className="introP">
+        <h2>This is Me</h2>
+        <p>
+          I have been a natural problem solver for my whole life. Some of my
+          earliest memories are playing puzzle game with my Dad. I have also
+          always been obsessed with computers. I remember watching coding videos
+          &#40;shoutout to{" "}
+          <a href="https://www.youtube.com/@TheCodingTrain">The Coding Train</a>
+          &#41; on YouTube before I even knew what a language was.
+        </p>
       </section>
     </main>
   );
