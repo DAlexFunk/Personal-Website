3d8812fc8cff8ad2e04df81cbb3998d1679f9764
DAlexFunk <alexdagreat04@gmail.com>
2024-11-27 11:30:25 -0500

    Feat: add the hero to the home page

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,12 +1,19 @@
 import alexImg from "../assets/alexHero.jpg";
+import arrow from "../assets/curvedArrow.svg"
 
 export default function Home() {
   return (
-    <main>
+    <main className="homePage">
       <section className="hero">
-        <h1>Alex Funk, Developer</h1>
-        <p>Creating Clean Code Since 2004 &#40;Not really&#41;</p>
-        <img src={alexImg} alt="" />
+        <div className="heroText">
+          <h1>Alex Funk, Developer</h1>
+          <p>Creating Clean Code Since 2004 &#40;Not really&#41;</p>
+        </div>
+        <div className="image">
+          <p className="name">That's Me</p>
+          <img src={arrow} className="arrow" />
+          <img src={alexImg} alt="Alex Funk" className="heroImg" />
+        </div>
       </section>
     </main>
   );
